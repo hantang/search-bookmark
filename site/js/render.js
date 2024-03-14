add7ea741c433aa190528bcd802b78fe48065178
hantang <34468720+hantang@users.noreply.github.com>
2024-03-15 00:53:47 +0800

    Update js

---
+++ site/js/render.js
@@ -0,0 +1,300 @@
+const openFileStr = 'file-input';
+const clearButtonStr = 'clear-button';
+const expandButtonStr = 'expand-button';
+const searchButtonStr = 'search-button';
+const resetButtonStr = 'reset-button';
+
+const searchOptionStr = 'search-options';
+const searchAreaStr = 'search-area';
+const searchLogicOptions = ["AND", "OR"];
+const searchFilterOptions = ["KEEP", "EXCLUDE"];
+const searchConditionOption = ['Highlight', 'Match Case', 'RegExp'];
+
+const bookmarkAreaStr = 'bookmark-area';
+const bookmarkTableStr = 'bookmark-table';
+const bookmarkCountStr = 'bookmark-count';
+const searchClassName = 'search-condition';
+
+function openBookmarkFile() {
+  const fileInput = document.getElementById(openFileStr);
+  fileInput.click();
+}
+
+// global
+function clearContent() {
+  document.getElementById(bookmarkTableStr).innerHTML = '';
+  document.getElementById(bookmarkCountStr).innerHTML = '';
+  document.getElementById(searchOptionStr).innerHTML = '';
+  document.getElementById(bookmarkAreaStr).style.display = 'none';
+  document.getElementById(searchAreaStr).style.display = 'none';
+}
+
+function renderPages(new_table, clear = true) {
+  console.log("Render pages")
+  if (clear) {
+    console.log("clearContent")
+    clearContent();
+  }
+
+  if (new_table === null) {
+    console.log("table is null")
+    return;
+  }
+  document.getElementById(bookmarkAreaStr).style.display = '';
+  document.getElementById(searchAreaStr).style.display = '';
+  
+  // displayTable(new_table);
+  const table = document.getElementById(bookmarkTableStr)
+  table.innerHTML = new_table.innerHTML;
+
+  const headers = table.getElementsByTagName("th");
+
+  updateTableCount(table);
+  const columns = [...headers].map(header => header.innerText);
+  const conditions = initSearchConditions(columns);
+  const searchOptions = document.getElementById(searchOptionStr);
+  searchOptions.appendChild(conditions);
+  for (let i = 0; i < headers.length; i++) {
+    headers[i].addEventListener("click", function () {
+      sortTable(i, table);
+    });
+  }
+};
+
+// other part
+function updateTableCount(table = null, cnt = null) {
+  let rowCount = 0;
+  if (cnt && cnt > 0) {
+    rowCount = cnt;
+  } else {
+    if (table == null)
+      table = document.getElementById(bookmarkTableStr);
+    if (table) {
+      rowCount = table.rows.length;
+      if (table.rows[0].cells[0].tagName === 'TH')
+        rowCount -= 1;
+    }
+  }
+
+  document.getElementById(bookmarkCountStr).innerHTML = Math.max(rowCount, 0);
+}
+
+function initSearchConditions(columns) {
+  // two parts
+  const className = searchClassName;
+  const searchInputClassName = 'search-condition-input';
+  const searchRadioClassName = 'search-condition-radio';
+  const searchCheckClassName = 'search-condition-check';
+
+  const searchConditions = document.createElement("div");
+
+  const fieldset1 = document.createElement('fieldset')
+  const part0 = document.createElement("div");
+  part0.className = className;
+  const checkboxes = createRadioOrCheckbox(searchConditionOption, searchCheckClassName, false, 'name-checkbox-input-condition')
+  part0.appendChild(checkboxes);
+  fieldset1.appendChild(part0);
+
+  const part1 = document.createElement("div");
+  part1.className = className;
+  const logicRadio = createRadioOrCheckbox(searchLogicOptions, searchRadioClassName, true, 'name-radio-input-logic')
+  part1.append(logicRadio);
+  fieldset1.appendChild(part1);
+  searchConditions.appendChild(fieldset1);
+
+  const fieldset2 = document.createElement('fieldset');
+  columns.forEach((label, index) => {
+    const container = document.createElement("div");
+    container.className = className;
+    const radioElement = createRadioOrCheckbox(searchFilterOptions, searchRadioClassName, true, `name-radio-input-filter-${index}`)
+    const inputElement = createTextInput(label, searchInputClassName, `name-text-input-${index}`);
+
+    container.appendChild(inputElement);
+    container.appendChild(radioElement);
+    if (index > 0) {
+      container.style.display = 'none';
+    }
+    fieldset2.append(container);
+  });
+  searchConditions.appendChild(fieldset2);
+  return searchConditions;
+}
+
+function toggleMoreSearchConditions() {
+  const className = searchClassName;
+  const searchOptions = document.querySelectorAll(`.${className}`);
+  const expandButton = document.getElementById(expandButtonStr);
+  console.log(searchOptions.length)
+  if (searchOptions.length > 3) {
+    if (searchOptions[3].style.display != '') {
+      for (let i = 3; i < searchOptions.length; i++) {
+        searchOptions[i].style.display = ''; // display
+        // TODO reset input
+      }
+      expandButton.innerText = 'Collapse';
+    } else {
+      for (let i = 3; i < searchOptions.length; i++) {
+        searchOptions[i].style.display = 'none'; // hidden
+      }
+      expandButton.innerText = 'Expand';
+    }
+  }
+
+}
+
+function filterTableData() {
+  // get checkboxes, radio buttons, text inputs
+  const className = searchClassName;
+  const searchArea = document.getElementById(searchAreaStr);
+  const checkboxes = document.querySelectorAll(`fieldset:nth-child(1) input[type="checkbox"]`);
+  const radios = document.querySelectorAll(`fieldset:nth-child(1) input[type="radio"]`);
+  const conditionsRaw = searchArea.querySelectorAll(`fieldset:nth-child(2) .${className}`);
+  const conditions = Array.from(conditionsRaw).filter(cond => cond.querySelector(".search-condition-input")
+    && cond.querySelector(".search-condition-input").value !== '');
+
+  const isHighlight = checkboxes[0].checked;
+  const isMatchCase = checkboxes[1].checked;
+  const isRegexp = checkboxes[2].checked;
+  const isLogicAnd = radios[0].checked;
+
+  const table = document.getElementById(bookmarkTableStr);
+  const rows = table.rows; // .getElementsByTagName("tr");
+  let start = 0;
+  if (rows.length > 0 && rows[0].cells[0].tagName === "TH") {
+    start = 1;
+  }
+  if (conditions.length == 0) {
+    updateTableCount(null, rows.length - start);
+    return;
+  }
+
+  resetTableData();
+  let cnt = 0;
+
+  for (let i = start; i < rows.length; i++) {
+    let displayRow = false;
+    let matchCount = 0;
+
+    conditions.forEach((cond) => {
+      // # TODO
+      const input = cond.querySelector(".search-condition-input");
+      const nameParts = input.name.split("-");
+      const columnIndex = parseInt(nameParts[nameParts.length - 1]);
+      const queryRaw = input.value;
+      const isLogicExclude = cond.querySelectorAll(".search-condition-radio")[1].checked; // keep/exclude
+      const td = rows[i].getElementsByTagName("td")[columnIndex];
+
+      const query = isRegexp ? queryRaw : queryRaw.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');;
+      const regex = new RegExp(query, isMatchCase ? "g" : "gi"); // i: ignore case
+
+      // Check if the condition matches the row
+      if (td && (
+        (!isLogicExclude && regex.test(td.textContent)) ||
+        (isLogicExclude && !regex.test(td.textContent)))) {
+        matchCount++;
+        if (isHighlight) {
+          const highlightedText = td.textContent.replace(
+            regex,
+            (match) => `<span style="background-color: yellow;">${match}</span>`
+          );
+          if (td.childElementCount == 0) {
+            td.innerHTML = highlightedText;
+          } else {
+            td.childNodes[0].innerHTML = highlightedText;
+          }
+        }
+      }
+    });
+
+    // Determine if the row should be displayed
+    if ((isLogicAnd && matchCount === conditions.length) ||
+      (!isLogicAnd && matchCount > 0)) {
+      displayRow = true;
+    }
+
+    // Update row display
+    rows[i].style.display = displayRow ? "" : "none";
+    if (displayRow) {
+      cnt += 1;
+    }
+  }
+  updateTableCount(null, cnt);
+}
+
+function resetTableData() {
+  const table = document.getElementById(bookmarkTableStr);
+  if (table === null) {
+    return
+  }
+  // show table rows
+  const trs = table.querySelectorAll('tr[style*="display: none"]');
+  trs.forEach(tr => {
+    tr.style.display = '';
+  });
+  // clean highlight
+  if (checkConditionOption(0)) {
+    const tds = table.querySelectorAll('td span');
+    tds.forEach(span => {
+      if (span.parentNode) {
+        span.parentNode.innerHTML = span.parentNode.innerText;
+      }
+    });
+  }
+  updateTableCount(table);
+}
+
+function resetSearchConditions() {
+  const className = searchClassName;
+  const checkboxes = document.querySelectorAll(`.${className} input[type="checkbox"]`);
+  checkboxes.forEach(checkbox => {
+    checkbox.checked = false;
+  });
+
+  // only first in each groups is checked
+  const firstRadioButtons = document.querySelectorAll(`.${className} input[type="radio"]:first-child`);
+  firstRadioButtons.forEach(radioButton => {
+    radioButton.checked = true;
+  });
+
+  const firstInputFields = document.querySelectorAll(`.${className} input[type="text"]`);
+  firstInputFields.forEach(input => {
+    input.value = "";
+  });
+}
+
+function resetSearchAndTable() {
+  resetTableData();
+  resetSearchConditions();
+
+}
+
+function checkConditionOption(index = 0) {
+  return document.querySelectorAll(`fieldset:nth-child(1) input[type="checkbox"]`)[index].checked;
+}
+
+// add events
+window.addEventListener('DOMContentLoaded', (event) => {
+  // check chrome extension api env
+  const extensionButton = document.getElementById('extension-button');
+  const openFileButton = document.getElementById('open-file-button');
+  const dropArea = document.getElementById('drop-area');
+  const fileInput = document.getElementById(openFileStr);
+
+  const isExtensionEnvironment = typeof chrome !== 'undefined' && chrome.extension;
+  if (isExtensionEnvironment) {
+    extensionButton.style.display = '';
+    extensionButton.addEventListener('click', loadBookmarkTree);
+  } else {
+    openFileButton.style.display = '';
+    dropArea.style.display = '';
+    dropArea.addEventListener('ondrop', dropHandler);
+    dropArea.addEventListener('ondragover', dragOverHandler);
+    openFileButton.addEventListener('click', openBookmarkFile);
+    fileInput.addEventListener('change', openSameFileAgain);
+  }
+
+  document.getElementById(clearButtonStr).addEventListener('click', clearContent);
+  document.getElementById(expandButtonStr).addEventListener('click', toggleMoreSearchConditions);
+  document.getElementById(searchButtonStr).addEventListener('click', filterTableData);
+  document.getElementById(resetButtonStr).addEventListener('click', resetSearchAndTable);
+});
---
+++ site/js/util.js
@@ -0,0 +1,262 @@
+// load and parse bookmark file
+// utils
+function isValidDate(dateString) {
+  // check column is a date column
+  // 2024-02-01, 2024-02-01 12:12:12, 2024-02-01T13:32:39.026Z
+  const pattern = /\d{4}-\d{2}-\d{2}([T ]\d{2}:\d{2}:\d{2}(\.\d*)?Z?)?/;
+  return dateString === '' || pattern.test(dateString);
+}
+
+function collectBookmarks(bookmarkNodes) {
+  let bookmarks = [];
+  function traverse(node, currentPath) {
+    if (node.url) {
+      bookmarks.push({ url: node.url, title: node.title, path: currentPath });
+    } else if (node.children) {
+      const newPath = currentPath + node.title + "/";
+      node.children.forEach(child => traverse(child, newPath));
+    }
+  }
+  bookmarkNodes.forEach(node => traverse(node, ""));
+  return bookmarks;
+}
+
+function getBookmarkDetails(bookmark) {
+  // chrome extension
+  return new Promise((resolve, reject) => {
+    chrome.history.getVisits({ url: bookmark.url }, function (visitItems) {
+      let visitCount = visitItems.length;
+      let lastVisitTime = visitCount > 0 ? new Date(visitItems[0].visitTime).toISOString() : null;
+      resolve({
+        ...bookmark,
+        visited: visitCount > 0,
+        lastVisitTime,
+        visitCount
+      });
+    });
+  });
+}
+
+function createBookmarkTable(bookmarks, hasVisited = true) {
+  const frequencyCounter = {};
+  for (const bookmark of bookmarks) {
+    const element = bookmark.url;
+    frequencyCounter[element] = (frequencyCounter[element] || 0) + 1;
+  }
+
+  const headers = ["Bookmark", "Website", "Folder", "FullFolder", "HasPath", "Duplicated"];
+  if (hasVisited) {
+    headers.push(...["LastVisitTime", "VisitCount"])
+  }
+
+  const table = document.createElement('table');
+  const thead = table.createTHead();
+  const tbody = table.createTBody();
+  const row = thead.insertRow();
+
+  headers.forEach(headerText => {
+    let header = document.createElement('th');
+    header.textContent = headerText;
+    row.appendChild(header);
+  });
+
+  bookmarks.forEach(bookmark => {
+    const title = bookmark.title.trim();
+    const href = bookmark.url;
+    const fullFolder = bookmark.path.endsWith("/") ? bookmark.path.slice(0, -1) : bookmark.path;
+    const folders = fullFolder.split("/")
+    const folder = folders ? folders[folders.length - 1] : '';
+
+    const url = new URL(href);
+    const hostname = url.hostname;
+    const hasPath = url.pathname && url.pathname !== '/';
+    const dup = frequencyCounter[href] > 1
+
+    const link = document.createElement('a');
+    link.href = url;
+    link.textContent = title;
+
+    const texts = [link, hostname, folder, fullFolder, hasPath, dup];
+    if (hasVisited) {
+      texts.push(...[bookmark.lastVisitTime, bookmark.visitCount]);
+    }
+    const tr = tbody.insertRow();
+    texts.forEach((text, idx) => {
+      if (idx == 0) {
+        const trCell = tr.insertCell();
+        trCell.appendChild(text);
+      } else {
+        tr.insertCell().textContent = text
+      }
+    });
+  });
+  return table
+}
+
+async function loadBookmarkTree() {
+  // chrome extension
+  try {
+    const bookmarkTree = await chrome.bookmarks.getTree();
+    const bookmarksData = collectBookmarks(bookmarkTree);
+    const bookmarkDetailsPromises = bookmarksData.map(bookmark => getBookmarkDetails(bookmark));
+    const bookmarks = await Promise.all(bookmarkDetailsPromises);
+    const table = createBookmarkTable(bookmarks, hasVisited = true);
+    renderPages(table);
+  } catch (error) {
+    console.error('Error processing bookmarks:', error);
+  }
+}
+
+function collectBookmarksFromFile(rootNode) {
+  let bookmarks = [];
+  function traverse(node, currentPath = '') {
+    const tag = node.tagName.toUpperCase();
+    let path = currentPath;
+    if (tag == 'DT') {
+      const folder = node.querySelector('h3');
+      const anchor = node.querySelector("a");
+      if (folder) {
+        path = currentPath + "/" + folder.textContent;
+      } else if (anchor) {
+        bookmarks.push({
+          url: anchor.href, title: anchor.textContent, path: path,
+        });
+      }
+    }
+    for (const child of node.children) {
+      traverse(child, path);
+    }
+  }
+  traverse(rootNode);
+  return bookmarks;
+}
+
+function loadBookmarkFile(file) {
+  if (file && file.type === 'text/html') {
+    const reader = new FileReader();
+    reader.onload = async function (event) {
+      const fileContent = event.target.result;
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(fileContent, 'text/html');
+      var table = doc.querySelector('table');
+
+      if (!table && doc.querySelectorAll('dl dt').length > 0) {
+        const rootNode = doc.querySelector('body');
+        const bookmarks = collectBookmarksFromFile(rootNode);
+        table = createBookmarkTable(bookmarks, hasVisited = false);
+      }
+      if (table) {
+        renderPages(table);
+      } else {
+        alert('No table or bookmarks found in the HTML file.');
+      }
+    };
+    reader.readAsText(file);
+  } else {
+    alert('Please drop an HTML file.');
+  }
+}
+
+// file drag and drop, open
+function dragOverHandler(event) {
+  event.preventDefault();
+}
+
+function dropHandler(event) {
+  event.preventDefault();
+  const file = event.dataTransfer.files[0];
+  loadBookmarkFile(file);
+}
+
+function openSameFileAgain(event) {
+  const selectedFile = event.target.files[0];
+  if (selectedFile) {
+    const file = event.target.files[0];
+    loadBookmarkFile(file);
+    event.target.value = ''; // allow open same file again
+  }
+}
+
+function createRadioOrCheckbox(itemList, className, isRadio = false, name = '') {
+  const container = document.createElement("div");
+  itemList.forEach((item, index) => {
+    const option = document.createElement('input');
+    option.className = className;
+    option.value = item;
+    option.type = isRadio ? 'radio' : 'checkbox';
+    option.name = name;
+    if (isRadio && index == 0) {
+      option.checked = true;
+    }
+
+    const label = document.createElement('label');
+    label.textContent = item;
+
+    container.appendChild(option);
+    container.appendChild(label);
+  });
+  return container;
+}
+
+function createTextInput(label, className, name) {
+  const container = document.createElement("div");
+  const label = document.createElement('label');
+  label.textContent = label + ': ';
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.name = name;
+  // label.toLowerCase().replace(/\s/g, '_');
+  input.className = className;
+
+  container.appendChild(label);
+  container.appendChild(input);
+  return container
+}
+
+// more
+function sortTable(column, table) {
+  // sort table depends on its type (string, number, date)
+  const rows = Array.from(table.getElementsByTagName("tr")).slice(1);
+  const isDateColumn = rows.every(row => isValidDate(row.cells[column].innerText));
+  const isNumericColumn = rows.every(row => !isNaN(parseFloat(row.cells[column].innerText)))
+
+  let sortOrder = 1;
+  const sortedColumn = table.getAttribute("data-sorted-column");
+  if (sortedColumn === String(column)) {
+    sortOrder = -parseInt(table.getAttribute("data-sort-order")) || 1;
+  } else {
+    const headers = table.getElementsByTagName("th");
+    for (let i = 0; i < headers.length; i++) {
+      headers[i].classList.remove("asc", "desc");
+    }
+  }
+
+  // Sort the rows based on the content of the specified column
+  rows.sort((rowA, rowB) => {
+    const cellA = rowA.cells[column].innerText;
+    const cellB = rowB.cells[column].innerText;
+    if (isDateColumn) {
+      const dateA = cellA === '' ? new Date(0) : new Date(cellA);
+      const dateB = cellB === '' ? new Date(0) : new Date(cellB);
+      return (dateA - dateB) * sortOrder;;
+    } else if (isNumericColumn) {
+      return (parseFloat(cellA) - parseFloat(cellB)) * sortOrder;
+    } else {
+      return cellA.localeCompare(cellB) * sortOrder;
+    }
+  });
+
+  // Reorder the rows in the table
+  for (let i = 0; i < rows.length; i++) {
+    table.appendChild(rows[i]);
+  }
+
+  // Update sorting indicator
+  const header = table.getElementsByTagName("th")[column];
+  header.classList.toggle("asc", sortOrder === 1);
+  header.classList.toggle("desc", sortOrder === -1);
+
+  // Store sorted column and order
+  table.setAttribute("data-sorted-column", column);
+  table.setAttribute("data-sort-order", sortOrder);
+}
